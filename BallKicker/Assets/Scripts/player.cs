3a038798f935a9cff234c15e7acd49e77b3c224a
Sebastian Ulrich <sesi.ulrich@gmail.com>
2020-06-25 15:04:07 +0200

    strucute created

---
+++ BallKicker/Assets/Scripts/player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private int movementSpeed = 80;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left")){
+            transform.position = new Vector3(movementSpeed * Time.deltaTime -1, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position = new Vector3(movementSpeed * Time.deltaTime +1, 0);
+        }
+    }
+}
